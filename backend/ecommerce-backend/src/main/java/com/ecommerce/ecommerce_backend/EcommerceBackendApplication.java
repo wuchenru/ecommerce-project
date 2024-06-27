f53532f082f2a6954a4d2f59311acf92c5cb4bc7
wuchenru <chenruwu31@gmail.com>
2024-06-27 11:28:06 -0700

    make the backend running with maven and springboot

--- backend/ecommerce-backend/src/main/java/com/ecommerce/ecommerce_backend/EcommerceBackendApplication.java
+++ backend/ecommerce-backend/src/main/java/com/ecommerce/ecommerce_backend/EcommerceBackendApplication.java
@@ -2,12 +2,18 @@ package com.ecommerce.ecommerce_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class EcommerceBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcommerceBackendApplication.class, args);
-	}
+	//public static void main(String[] args) {
+		// SpringApplication.run(EcommerceBackendApplication.class, args);
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+	
 
 }
